54b4064ebbdd407b14751aa85813df6e1595e19d
Asif Newaz <asifnewaz.cste@gmail.com>
2019-10-17 00:59:56 +0600

    Acitve Label to make UILabel Clickable

--- ActiveLabel/Usage.swift
+++ ActiveLabel/Usage.swift
@@ -2,20 +2,33 @@
 //2. Add class of the label --- ActiveLable
 //3. Hook the label with viewController
 //4. then add the following code in viewDidLoad
+//5. Add UICiewController Extension in project
 
 //MARK: Active Label
+makeLabelActive(label: self.termsAndConditionsLabel)
+
+
+// Extension
+extension UIViewController {
+    func makeLabelActive(label: ActiveLabel){
         let customType = ActiveType.custom(pattern: "\\Terms and Conditions\\b")
-        termsAndConditionsLabel.enabledTypes.append(customType)
-        termsAndConditionsLabel.urlMaximumLength = 31
-        termsAndConditionsLabel.customize { termsAndConditionsLabel in
-            termsAndConditionsLabel.text = "I/We acknowledge and accept the Terms and Conditions applicable and available."
-            termsAndConditionsLabel.numberOfLines = 2
-            termsAndConditionsLabel.lineSpacing = 0
+        label.enabledTypes.append(customType)
+        label.urlMaximumLength = 31
+        label.customize { label in
+            label.text = "I/We acknowledge and accept the Terms and Conditions applicable and available."
+            label.numberOfLines = 2
+            label.lineSpacing = 0
+
             //Custom types
-            termsAndConditionsLabel.customColor[customType] = UIColor.systemBlue
-            termsAndConditionsLabel.customSelectedColor[customType] = UIColor.systemBlue
-                                           
-            termsAndConditionsLabel.handleCustomTap(for: customType){ _ in 
+            label.customColor[customType] = UIColor.systemBlue
+            label.customSelectedColor[customType] = UIColor.systemBlue
+            label.handleCustomTap(for: customType){ _ in
                 print("Link Tapped")
+                let storyboard = UIStoryboard(name: "others", bundle: Bundle.main)
+                let passwordVC = storyboard.instantiateViewController(withIdentifier: "PrivacyPolicyViewController") as? PrivacyPolicyViewController
+                passwordVC?.url = AppGlobal.about
+                self.navigationController?.show(passwordVC!, sender: self)
             }
         }
+    }
+}
