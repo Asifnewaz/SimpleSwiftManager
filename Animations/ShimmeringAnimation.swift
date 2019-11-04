e8d040d756ba19fc364b75eff0d9e30dd6a41a1c
Asif Newaz <asifnewaz.cste@gmail.com>
2019-11-04 15:25:15 +0600

    Facebook like Shimmering Animation for UIView.

---
+++ Animations/ShimmeringAnimation.swift
@@ -0,0 +1,29 @@
+
+
+extension UIView {
+
+  func startShimmering(){
+        let light = UIColor.white.cgColor
+        let alpha = UIColor(red: 206/255, green: 10/255, blue: 10/255, alpha: 0.7).cgColor //UIColor.white.withAlphaComponent(0.7).cgColor
+        
+        let gradient = CAGradientLayer()
+        gradient.colors = [light, alpha, light]
+        gradient.frame = CGRect(x: -self.bounds.size.width, y: 0, width: 3 * self.bounds.size.width, height: self.bounds.size.height)
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.525)
+        gradient.locations = [0.4, 0.5, 0.6]
+        self.layer.mask = gradient
+        
+        let animation = CABasicAnimation(keyPath: "locations")
+        animation.fromValue = [0.0, 0.1, 0.2]
+        animation.toValue = [0.8, 0.9, 1.0]
+        animation.duration = 1.3
+        animation.repeatCount = HUGE
+        gradient.add(animation, forKey: "shimmer")
+    }
+    
+    func stopShimmering(){
+        self.layer.mask = nil
+    }
+    
+}
