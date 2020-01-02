5678804c7e0e79401535d9018a5475629cad3775
Asif Newaz <asifnewaz.cste@gmail.com>
2020-01-02 11:06:25 +0600

    Horizontal Dahsed UIView
    -------- Horizontal dahsed view for storyboard

---
+++ DesignableClass/HorizontalDashedUIView.swift
@@ -0,0 +1,56 @@
+//
+//  DashedLineUIView.swift
+//  CashBaba
+//
+//  Created by Asif Newaz on 29/10/19.
+//  Copyright © 2019 Recursion Technologies Ltd. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class DashedLineView : UIView {
+    
+    @IBInspectable var perDashLength: CGFloat = 2.0
+    
+    @IBInspectable var spaceBetweenDash: CGFloat = 2.0
+    
+    @IBInspectable var dashColor: UIColor = UIColor.lightGray
+
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        let  path = UIBezierPath()
+        if height > width {
+            let  p0 = CGPoint(x: self.bounds.midX, y: self.bounds.minY)
+            path.move(to: p0)
+
+            let  p1 = CGPoint(x: self.bounds.midX, y: self.bounds.maxY)
+            path.addLine(to: p1)
+            path.lineWidth = width
+
+        } else {
+            let  p0 = CGPoint(x: self.bounds.minX, y: self.bounds.midY)
+            path.move(to: p0)
+
+            let  p1 = CGPoint(x: self.bounds.maxX, y: self.bounds.midY)
+            path.addLine(to: p1)
+            path.lineWidth = height
+        }
+
+        let  dashes: [ CGFloat ] = [ perDashLength, spaceBetweenDash ]
+        path.setLineDash(dashes, count: dashes.count, phase: 0.0)
+
+        path.lineCapStyle = .butt
+        dashColor.set()
+        path.stroke()
+    }
+
+    private var width : CGFloat {
+        return self.bounds.width
+    }
+
+    private var height : CGFloat {
+        return self.bounds.height
+    }
+}
